0b6408384184ccb63c6dbd240ca040503f168bc0
Nick Mardon <nickmardondigital@gmail.com>
2020-06-18 22:21:03 -0700

    javascript connected, click listener on save button

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //Pseudo Code 
 
-// 1. Select elements from HTML
-var saveBtn;
+// TODO: Select elements from HTML
+var saveBtn = $(".saveBtn");
 var todoInput = $("textarea").value;
 var  timeBlock; 
+
+//TODO: when submit button is clicked save the value of the text area to local storage
+
+
+saveBtn.on("click", function(){ 
+    console.log("clicked")
+});
